4cc9eef50b31427e3e59161c2d0223a6b1846642
mattia.fazzini <mattia.fazzini@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2009-08-07 15:27:35 +0000

    Chemical Field Create 3D Update ATTENTION:parameter file changed

--- trunk/bsim/drawable/field/BSimChemicalFieldCreate.java
+++ trunk/bsim/drawable/field/BSimChemicalFieldCreate.java
@@ -20,19 +20,21 @@ public class BSimChemicalFieldCreate {
 	                                                     Color col, BSimParameters params) {
 		
 		// Create an array for the start position
-		double[] startPos = new double[2];
+		double[] startPos = new double[3];
 		startPos[0] = define[0];
 		startPos[1] = define[1];
+		startPos[1] = define[2];
 		
 		// Create the new chemical field
-		BSimChemicalField newField = new BSimChemicalField((int)define[7], 
-											(int)define[8], define[9], startPos, define[2], 
-											define[3], (int)define[4], (int)define[5], 
-											params.getDtSecs(), define[5], col, params);
+		BSimChemicalField newField = new BSimChemicalField((int)define[10], 
+											(int)define[11], define[12], startPos, define[3], 
+											define[4],	define[5], (int)define[6], (int)define[7], (int)define[8],
+											params.getDtSecs(), define[9], col, params);
 		
 		// Update if the field has an initial distribution
 		if(setup[1] == 1) { newField.setAsLinear(BSimChemicalField.LINEAR_X, setup[2], setup[3]); }
 		if(setup[1] == 2) { newField.setAsLinear(BSimChemicalField.LINEAR_Y, setup[2], setup[3]); }
+		if(setup[1] == 3) { newField.setAsLinear(BSimChemicalField.LINEAR_Z, setup[2], setup[3]); }
 		
 		// Update the field visibility
 		if(setup[0] == 1) { newField.setDisplayed(true); }
