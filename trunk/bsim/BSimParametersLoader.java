e47294c9ed07a860c1be12c0b95d7cdd91f224ea
mattia.fazzini <mattia.fazzini@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2009-08-08 12:41:44 +0000

    Changed the string to load the parameters for the solid box

--- trunk/bsim/BSimParametersLoader.java
+++ trunk/bsim/BSimParametersLoader.java
@@ -98,7 +98,7 @@ public class BSimParametersLoader {
 		else if(line[0].equals("PHYSICS_WELL_DEPTH_BACT_BDRY:")) p.setWellDepthBactBdry((double)args[0]);
 		else if(line[0].equals("PHYSICS_REACT_FORCE:")) p.setReactForce((double)args[0]);
 		
-		else if(line[0].equals("CREATE_BOUNDARY_SOLID:")) p.addSolidBoundary(args);
+		else if(line[0].equals("CREATE_BOUNDARY_SOLID_BOX:")) p.addSolidBoundary(args);
 		else if(line[0].equals("CREATE_BOUNDARY_WRAP:")) p.addWrapBoundary(args);
 		
 		else if(line[0].equals("FIELD_GOAL_DEFINE:")) p.setCfGoalDefine(args);
