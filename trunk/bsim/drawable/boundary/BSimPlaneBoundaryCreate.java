f846550c89e8e178a0fad7eb8e644fcd67048b89
mattia.fazzini <mattia.fazzini@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2009-08-09 16:09:56 +0000

    Updated method to create the new Wrap Plane

--- trunk/bsim/drawable/boundary/BSimPlaneBoundaryCreate.java
+++ trunk/bsim/drawable/boundary/BSimPlaneBoundaryCreate.java
@@ -117,26 +117,112 @@ public class BSimPlaneBoundaryCreate {
 	}
 	
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	/**
 	 * Create a wrapping boundary from parameter arguments
 	 */
-	public static BSimWrapBoundary createWrapBoundary( double[] args ){
+	public static BSimWrapPlaneBoundary createWrapPlaneBoundary( double[] args, int j){
 		
 		// Create points required to create the solid boundary
-		double[] p1 = new double[2];
-		double[] p2 = new double[2];
-		double[] offset = new double[2];
+		double[] p1 = new double[3];
+		double[] p2 = new double[3];
+		double[] p3 = new double[3];
+		double[] p4 = new double[3];
 		
-		p1[0] = args[0];
-		p1[1] = args[1];
-		p2[0] = args[2];
-		p2[1] = args[3];
-		offset[0] = args[4];
-		offset[1] = args[5];
+		
+		if(j==0){
+			p1[0] = args[0];
+			p1[1] = args[1];
+			p1[2] = args[2];
+			p2[0] = args[0]+args[3];
+			p2[1] = args[1];
+			p2[2] = args[2];
+			p3[0] = args[0]+args[3];
+			p3[1] = args[1]+args[4];
+			p3[2] = args[2];
+			p4[0] = args[0];
+			p4[1] = args[1]+args[4];
+			p4[2] = args[2];	
+		}
+		else if (j==1){
+			p1[0] = args[0];
+			p1[1] = args[1];
+			p1[2] = args[2]+args[5];
+			p2[0] = args[0]+args[3];
+			p2[1] = args[1];
+			p2[2] = args[2]+args[5];
+			p3[0] = args[0]+args[3];
+			p3[1] = args[1];
+			p3[2] = args[2];
+			p4[0] = args[0];
+			p4[1] = args[1];
+			p4[2] = args[2];
+			
+		}
+		else if (j==2){
+			p1[0] = args[0];
+			p1[1] = args[1];
+			p1[2] = args[2]+args[5];
+			p2[0] = args[0]+args[3];
+			p2[1] = args[1];
+			p2[2] = args[2]+args[5];
+			p3[0] = args[0]+args[3];
+			p3[1] = args[1]+args[4];
+			p3[2] = args[2]+args[5];
+			p4[0] = args[0];
+			p4[1] = args[1]+args[4];
+			p4[2] = args[2]+args[5];
+		}
+		else if (j==3){
+			p1[0] = args[0];
+			p1[1] = args[1]+args[4];
+			p1[2] = args[2]+args[5];
+			p2[0] = args[0]+args[3];
+			p2[1] = args[1]+args[4];
+			p2[2] = args[2]+args[5];
+			p3[0] = args[0]+args[3];
+			p3[1] = args[1]+args[4];
+			p3[2] = args[2];
+			p4[0] = args[0];
+			p4[1] = args[1]+args[4];
+			p4[2] = args[2];
+			
+		}
+		else if (j==4){
+			p1[0] = args[0];
+			p1[1] = args[1];
+			p1[2] = args[2]+args[5];
+			p2[0] = args[0];
+			p2[1] = args[1]+args[4];
+			p2[2] = args[2]+args[5];
+			p3[0] = args[0];
+			p3[1] = args[1]+args[4];
+			p3[2] = args[2];
+			p4[0] = args[0];
+			p4[1] = args[1];
+			p4[2] = args[2];
+		}
+		else{
+			p1[0] = args[0]+args[3];
+			p1[1] = args[1];
+			p1[2] = args[2]+args[5];
+			p2[0] = args[0]+args[3];
+			p2[1] = args[1]+args[4];
+			p2[2] = args[2]+args[5];
+			p3[0] = args[0]+args[3];
+			p3[1] = args[1]+args[4];
+			p3[2] = args[2];
+			p4[0] = args[0]+args[3];
+			p4[1] = args[1];
+			p4[2] = args[2];
+		}
+		
+		double[] newWrapOffset= {0.0,0.0,0.0};
+		newWrapOffset[0]=args[6];
+		newWrapOffset[1]=args[7];
+		newWrapOffset[2]=args[8];		
 		
 		// return the new boundary
-		return new BSimWrapBoundary(p1, p2, offset, args[6]);
+		return new BSimWrapPlaneBoundary(p1, p2, p3, p4, args[3], args[4], args[5], newWrapOffset, j, args[9]);
 	}
 }
 
