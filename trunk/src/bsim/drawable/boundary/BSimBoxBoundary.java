3f37cf0ac091a79796eff0c40e37ce61d9f257db
mattia.fazzini <mattia.fazzini@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2009-08-17 10:15:34 +0000

    Class that represent the box boundary used in the 3d rendering.(both wrap and solid)

---
+++ trunk/src/bsim/drawable/boundary/BSimBoxBoundary.java
@@ -0,0 +1,68 @@
+/**
+ * BSimSolidBoxBoundary.java
+ *
+ * Class that represents a box of the simulation boundaries.
+ *
+ * Authors: Mattia Fazzini
+ * Created: 13/08/2009
+ */
+package bsim.drawable.boundary;
+
+import java.awt.Color;
+
+import java.awt.Graphics;
+
+import bsim.drawable.BSimDrawable;
+
+
+public class BSimBoxBoundary implements BSimDrawable {
+
+	
+	//plane points
+	protected double[] p1 = {0.0,0.0,0.0};
+	
+	protected double length = 0;
+	protected double width = 0;
+	protected double depth = 0;
+	
+	/**
+	 * General constructor.
+	 */
+	public BSimBoxBoundary(double[] args) {
+		
+		// Update the internal variables
+		p1[0] = args[0];
+		p1[1] = args[1];
+		p1[2] = args[2];
+		
+		length = args[3];
+		width = args[4];
+		depth = args[5];
+	}	
+	
+	/**
+	 * Standard get methods
+	 */
+	public double[] getP1() { return p1; }
+	public double getLength() { return length; }
+	public double getWidth() { return width; }
+	public double getDepth() { return depth; }
+	
+	/**
+	 * Method to return the centrePos of the box
+	 */
+	public double[] getCentrePos() {
+		double[] centrePos= {0.0,0.0,0.0};
+		centrePos[0]=p1[0]+(length/2);
+		centrePos[1]=p1[1]+(width/2);
+		centrePos[2]=p1[2]+(depth/2);
+		return centrePos; 
+		}
+	
+	
+	/**
+	 * Draw the boundary.
+	 */
+	public void redraw(Graphics g) {
+	}
+}
