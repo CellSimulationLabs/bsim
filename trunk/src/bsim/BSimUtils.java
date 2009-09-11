ad9a159a9e58cd375b381ea7214f62b1638d0e3c
postscript07 <postscript07@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2009-09-11 14:42:10 +0000

    BSimUtils

--- trunk/src/bsim/BSimUtils.java
+++ trunk/src/bsim/BSimUtils.java
@@ -12,6 +12,7 @@ package bsim;
 
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Random;
 
 
 
@@ -28,27 +29,50 @@ public class BSimUtils {
 		return c/v.size();
 	}
 	
-		
+			
 	/**
-	* Returns an exponentially distributed random variable.
+	* Returns gamma distributed random variable
+	* http://vyshemirsky.blogspot.com/2007/11/sample-from-gamma-distribution-in-java.html
 	*/
-	public static synchronized double expRandVar(double mean) {
-		return -mean * Math.log(1-Math.random());
-	}
-	
-	
-	/**
-	* Returns approximate gamma distributed random variable (Parameters for bacteria).
-	*/
-	public static synchronized double approxGammaRandVar() {
-		double x = (Math.random()*2.0)-1;
-		if(x>0) {
-			return (221.64*Math.pow(x,4.0)) - (109.34*Math.pow(x,3.0)) - 
-			(137.84*Math.pow(x,2.0) + (177.58*x) + 12.736);
-		}
-		else {
-			return -(221.64*Math.pow(-x,4.0)) - (109.34*Math.pow(-x,3.0)) - 
-			(137.84*Math.pow(-x,2.0) + (177.58*-x) + 12.736);
+	public static double sampleGamma(double k, double theta) {
+		Random rng = new Random();
+		boolean accept = false;
+		if (k < 1) {
+			// Weibull algorithm
+			double c = (1 / k);
+			double d = ((1 - k) * Math.pow(k, (k / (1 - k))));
+			double u, v, z, e, x;
+			do {
+				u = rng.nextDouble();
+				v = rng.nextDouble();
+				z = -Math.log(u);
+				e = -Math.log(v);
+				x = Math.pow(z, c);
+				if ((z + e) >= (d + x)) {
+					accept = true;
+				}
+			} while (!accept);
+			return (x * theta);
+		} else {
+			// Cheng's algorithm
+			double b = (k - Math.log(4));
+			double c = (k + Math.sqrt(2 * k - 1));
+			double lam = Math.sqrt(2 * k - 1);
+			double cheng = (1 + Math.log(4.5));
+			double u, v, x, y, z, r;
+			do {
+				u = rng.nextDouble();
+				v = rng.nextDouble();
+				y = ((1 / lam) * Math.log(v / (1 - v)));
+				x = (k * Math.exp(y));
+				z = (u * v * v);
+				r = (b + (c * y) - x);
+				if ((r >= ((4.5 * z) - cheng)) ||
+						(r >= Math.log(z))) {
+					accept = true;
+				}
+			} while (!accept);
+			return (x * theta);
 		}
 	}
 		
