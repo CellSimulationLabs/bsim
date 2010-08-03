f13f422379e47ee5cbad236c5d4d1a826591c9ba
antmatyjajo <antmatyjajo@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2010-08-03 23:11:55 +0000

    Some mesh bits and bobs as they stand. MeshTest.java has a couple of sample implementations. Please play and let me know if this is a good representation (coding wise) for interacting nicely with other features. Mostly still a work in progress...

---
+++ trunk/src/bsim/mesh/BSimVertex.java
@@ -0,0 +1,50 @@
+/**
+ * BSimVertex.java
+ * A mesh vertex.
+ */
+
+package bsim.mesh;
+
+import java.util.ArrayList;
+
+import javax.vecmath.Point3d;
+
+/**
+ *  Mesh vertex.
+ *  Defines the location of a mesh vertex in 3-D space as a Point3d,
+ *  and also stores a cached list of the faces which are attached to
+ *  this vertex.
+ */
+public class BSimVertex {
+	/**
+	 * The Cartesian coordinates of the vertex in 3-D space.
+	 */
+	protected Point3d location;
+	
+	/**
+	 * List of the indices of the faces which use this vertex.
+	 * Currently this is not updated from within BSimVertex but from the
+	 * surface mesh implementation (e.g BSimFVMesh).
+	 */
+	protected ArrayList<Integer> faces = new ArrayList<Integer>(1);
+	
+	/**
+	 * Constructor: create a new mesh vertex from three points; x,y,z
+	 */
+	public BSimVertex(double newX, double newY, double newZ){
+		Point3d newLocation = new Point3d(newX,newY,newZ);
+		location = newLocation;
+	}
+	
+	/**
+	 * Constructor: create a new mesh vertex from a Point3d
+	 */
+	public BSimVertex(Point3d newLocation){
+		location = newLocation;
+	}
+	
+	// Parameter getters.
+	public Point3d getLocation(){ return location;}
+	public ArrayList<Integer> getFaces(){ return faces;}
+}
+
