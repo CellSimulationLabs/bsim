55a2a9a78b40af2f5384ded134ec47d2a82161bb
antmatyjajo <antmatyjajo@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2010-09-22 17:29:24 +0000

    Added methods for testing intersection between triangle and octree node, triangle and axis aligned box, triangle and plane. Needs lots of testing to make sure it all works correctly.

--- trunk/src/bsim/geometry/BSimMeshUtils.java
+++ trunk/src/bsim/geometry/BSimMeshUtils.java
@@ -10,9 +10,11 @@
 
 package bsim.geometry;
 
-import javax.vecmath.Point3d;
+import java.util.Vector;
+
 import javax.vecmath.Vector3d;
 
+import bsim.OctreeNode;
 import bsim.particle.BSimParticle;
 
 public class BSimMeshUtils {
@@ -26,7 +28,7 @@ public class BSimMeshUtils {
 	 * @param p the point of intersection
 	 * @return true = the sphere does intersect the triangle
 	 */
-	public static boolean intersectSphereTriangle(BSimParticle s, Point3d a, Point3d b, Point3d c, Point3d p){
+	public static boolean intersectSphereTriangle(BSimParticle s, Vector3d a, Vector3d b, Vector3d c, Vector3d p){
 		// p = point on triangle closest to the sphere centre
 		p.set(closestPtPointTriangle(s.getPosition(),a,b,c));
 
@@ -45,7 +47,7 @@ public class BSimMeshUtils {
 	 * @param c Triangle vertex c (v2)
 	 * @return
 	 */
-	public static Point3d closestPtPointTriangle(Vector3d p, Point3d a, Point3d b, Point3d c){
+	public static Vector3d closestPtPointTriangle(Vector3d p, Vector3d a, Vector3d b, Vector3d c){
 		// Check if P in vertex region outside A
 		Vector3d ab = new Vector3d(); ab.sub(b,a);
 		Vector3d ac = new Vector3d(); ac.sub(c,a);
@@ -61,7 +63,7 @@ public class BSimMeshUtils {
 		double d4 = ac.dot(bp);
 		if (d3 >= 0.0 && d4 <= d3) return b; // Vertex b (barycentric coordinates (0,1,0))
 
-		Point3d rTemp = new Point3d();
+		Vector3d rTemp = new Vector3d();
 
 		// Check if P in edge region of AB, if so return projection of P onto AB
 		double vc = d1*d4 - d3*d2;
@@ -186,37 +188,217 @@ public class BSimMeshUtils {
 	}
 	
 	/**
-	 * Intersection of a vector in 3d space with a plane
+	 * Test for intersection of a triangle against an octree node
+	 * @param t BSimTriangle to test
+	 * @param n Octree node to test against
+	 * @return true if an intersection occurs
 	 */
-	public static void intersectVectorPlane(){
-		
+	public static boolean intersectTriOctreeNode(BSimTriangle t, OctreeNode n){
+		double length = n.getLength();
+		return intersectTriangleAAB(t, n.getCentre(), new Vector3d(length, length, length));
 	}
 	
 	/**
-	 * Intersection of a vector (line segment) with a BSimCollisionOctree
+	 * Test for intersection of a triangle against an axis aligned box
+	 * @param t BSimTriangle to test
+	 * @param boxCentre Centre coordinate of the box
+	 * @param boxDim Box length in x,y,z directions 
+	 * @return true if an intersection occurs
 	 */
-	public static void intersectVectorOctree(){
+	public static boolean intersectTriangleAAB(BSimTriangle t, Vector3d boxCentre, Vector3d boxDim){
+		// Algorithm based on 'Real Time Rendering 3rd Ed.' pp 760 (based on a separating axes test)
+		// Ideas for optimisation based on C code from the author's website: 
+		// http://www.cs.lth.se/home/Tomas_Akenine_Moller/code/
+		// Beastly long method, sorry...
 
+		// Box extents away from the centre (half lengths)
+		double hLengthX = boxDim.x * 0.5;
+		double hLengthY = boxDim.y * 0.5;
+		double hLengthZ = boxDim.z * 0.5;
+
+		// Triangle vertices
+		Vector3d v0 = new Vector3d(t.getTCoords(0));
+		Vector3d v1 = new Vector3d(t.getTCoords(1));
+		Vector3d v2 = new Vector3d(t.getTCoords(2));
+
+		// Translate triangle - (conceptually moving box to the origin)
+		v0.sub(boxCentre);
+		v1.sub(boxCentre); 
+		v2.sub(boxCentre);
+
+		// Compute edge vectors for triangle
+		Vector3d e0 = new Vector3d();
+		Vector3d e1 = new Vector3d();
+		Vector3d e2 = new Vector3d();
+		e0.sub(v1, v0);
+		e1.sub(v2, v1);
+		e2.sub(v0, v2);
+
+		// Test the 9 separating axes
+		// Yup they're all inlined because its simpler to use our local variables this way...doh!
+		double p0, p1, p2, radius;
+		double min, max;
+		double fex = Math.abs(e0.getX());
+		double fey = Math.abs(e0.getY());
+		double fez = Math.abs(e0.getZ());
+
+		// Begin horrible code.....
+			p0 = e0.getZ()*v0.getY() - e0.getY()*v0.getZ();			       	    
+			p2 = e0.getZ()*v2.getY() - e0.getY()*v2.getZ();			       	    
+		        if(p0 < p2){
+		        	min = p0; max = p2;
+		        }else{
+		        	min = p2; max = p0;
+		        }  
+			radius = fez * hLengthY + fey * hLengthZ;    
+			if(min > radius || max < -radius) return false;
+			
+			p0 = -e0.getZ()*v0.getX() + e0.getX()*v0.getZ();		      	    
+			p2 = -e0.getZ()*v2.getX() + e0.getX()*v2.getZ();	       	       	    
+		        if(p0 < p2){
+		        	min = p0; max = p2;
+		        }else{
+		        	min = p2; max = p0;
+		        }  
+			radius = fez * hLengthX + fex * hLengthZ;    
+			if(min > radius || max < -radius) return false;
+			
+			p1 = e0.getY()*v1.getX() - e0.getX()*v1.getY();			            
+			p2 = e0.getY()*v2.getX() - e0.getX()*v2.getY();			       	    
+		        if(p2 < p1){
+		        	min = p2; max = p1;
+		        }else{
+		        	min = p1; max = p2;
+		        }  
+			radius = fey * hLengthX + fex * hLengthY;    
+			if(min > radius || max < -radius) return false;
+			
+			
+			fex = Math.abs(e1.getX());
+			fey = Math.abs(e1.getY());
+			fez = Math.abs(e1.getZ());
+			
+			p0 = e1.getZ()*v0.getY() - e1.getY()*v0.getZ();			       	    
+			p2 = e1.getZ()*v2.getY() - e1.getY()*v2.getZ();			       	    
+		        if(p0 < p2){
+		        	min = p0; max = p2;
+		        } else {
+		        	min = p2; max = p0;
+		        }  
+			radius = fez * hLengthY + fey * hLengthZ;    
+			if(min > radius || max < -radius) return false;
+			
+			p0 = -e1.getZ()*v0.getX() + e1.getX()*v0.getZ();		      	    
+			p2 = -e1.getZ()*v2.getX() + e1.getX()*v2.getZ();	       	       	    
+		        if(p0 < p2) {
+		        	min = p0; max = p2;
+		        } else {
+		        	min = p2; max = p0;
+		        }  
+			radius = fez * hLengthX + fex * hLengthZ;    
+			if(min > radius || max < -radius) return false;
+	
+			p0 = e1.getY()*v0.getX() - e1.getX()*v0.getY();				    
+			p1 = e1.getY()*v1.getX() - e1.getX()*v1.getY();			            
+		        if(p0 < p1){
+		        	min = p0; max = p1;
+		        }else{
+		        	min = p1; max = p0;
+		        }
+			radius = fey * hLengthX + fex * hLengthY;    
+			if(min > radius || max < -radius) return false;
+			
+			fex = Math.abs(e2.getX());
+			fey = Math.abs(e2.getY());
+			fez = Math.abs(e2.getZ());
+			
+			p0 = e2.getZ()*v0.getY() - e2.getY()*v0.getZ();			            
+			p1 = e2.getZ()*v1.getY() - e2.getY()*v1.getZ();			       	    
+		        if(p0 < p1){
+		        	min = p0; max = p1;
+		        }else{
+		        	min = p1; max = p0;
+		        }  
+			radius = fez * hLengthY + fey * hLengthZ;    
+			if(min>radius || max<-radius) return false;
+			
+			p0 = -e2.getZ()*v0.getX() + e2.getX()*v0.getZ();		      	    
+			p1 = -e2.getZ()*v1.getX() + e2.getX()*v1.getZ();	     	       	    
+		        if(p0 < p1){
+		        	min = p0; max = p1;
+		        }else{
+		        	min = p1; max = p0;
+		        }  
+			radius = fez * hLengthX + fex * hLengthZ;    
+			if(min > radius || max < -radius) return false;
+			
+			p1 = e2.getY()*v1.getX() - e2.getX()*v1.getY();			            
+			p2 = e2.getY()*v2.getX() - e2.getX()*v2.getY();			       	    
+		        if(p2 < p1){
+		        	min = p2; max = p1;
+		        }else{
+		        	min = p1; max = p2;
+		        }  
+			radius = fey * hLengthX + fex * hLengthY;    
+			if(min > radius || max < -radius) return false;
+		// End horrible code...
+
+		// check RTCD pp.169
+		// Test the three axes corresponding to the face normals of box
+		// Exit if...
+		// ... [-lengthX, lengthX] and [min(v0.x,v1.x,v2.x), max(v0.x,v1.x,v2.x)] do not overlap
+		if (Math.max(v0.x, Math.max(v1.x, v2.x)) < -hLengthX || Math.min(v0.x, Math.min(v1.x, v2.x)) > hLengthX) return false;
+		// ... [-lengthY, lengthY] and [min(v0.y,v1.y,v2.y), max(v0.y,v1.y,v2.y)] do not overlap
+		if (Math.max(v0.y, Math.max(v1.y, v2.y)) < -hLengthY || Math.min(v0.y, Math.min(v1.y, v2.y)) > hLengthY) return false;
+		// ... [-lengthZ, lengthZ] and [min(v0.z,v1.z,v2.z), max(v0.z,v1.z,v2.z)] do not overlap
+		if (Math.max(v0.z, Math.max(v1.z, v2.z)) < -hLengthZ || Math.min(v0.z, Math.min(v1.z, v2.z)) > hLengthZ) return false;
+		
+		// Test separating axis corresponding to triangle face normal
+		
+		Vector3d normal = t.getNormal();
+		double d = normal.dot(v0);
+		
+		return intersectPlaneAAB(normal, d, boxCentre, boxDim);
 	}
+	
+	/**
+	 * Test for intersection between a plane and an axis-aligned box.
+	 * @param normal Plane normal
+	 * @param d Plane d value (from plane equation, d = normal.dot(v0)) where v0 is some point on the plane
+	 * @param boxCentre Box centre coordinates
+	 * @param boxDim Box side lengths in x,y,z directions
+	 * @return true if an intersection occurs
+	 */
+	public static boolean intersectPlaneAAB(Vector3d normal, double d, Vector3d boxCentre, Vector3d boxDim)
+	{
+		Vector3d e = new Vector3d(boxDim);
+		e.scale(0.5);
+		e.absolute(); // Just in case.
+		
+		// Compute the projection interval radius of b onto L(t) = b.c + t * p.n
+		double r = e.getX()*Math.abs(normal.getX()) 
+					+ e.getY()*Math.abs(normal.getY()) 
+					+ e.getZ()*Math.abs(normal.getZ());
+		
+		// Compute distance of box centre from plane
+		double s = normal.dot(boxCentre) - d;
+		
+		// Intersection occurs when distance s falls within [-r,+r] interval
+		return (Math.abs(s) <= r);
+	}
+	
 	
 	/**
 	 * Intersection of a vector with an axis aligned bounding box
 	 */
-	public static void intersectVectorAABB(){
-
-	}
-	
-	/**
-	 * Intersection of a sphere (BSimParticle) with a BSimCollisionOctree
-	 */
-	public static void intersectSphereOctree(){
+	public static void intersectVectorAAB(){
 
 	}
 	
 	/**
 	 * Intersection of a sphere with and AABB
 	 */
-	public static void intersectSphereAABB(){
+	public static void intersectSphereAAB(){
 
 	}
 	
@@ -225,6 +407,13 @@ public class BSimMeshUtils {
 	 * Test if a point in 3d space is inside a <b>*convex*</b> polyhedron.
 	 */
 	public static void pointInPolyhedron(){
+		
+	}
+	
+	/**
+	 * Intersection of a vector in 3d space with a plane
+	 */
+	public static void intersectVectorPlane(){
 		
 	}
 	
