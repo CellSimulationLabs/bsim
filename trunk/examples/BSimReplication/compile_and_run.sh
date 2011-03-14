9ca2f6742fae2c5882a832358b37d03e8b43d089
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-03-14 13:51:04 +0000

    Updated examples

--- trunk/examples/BSimReplication/compile_and_run.sh
+++ trunk/examples/BSimReplication/compile_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-javac -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimBrownianMotion.java
-java  -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimBrownianMotion
+javac -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimReplication.java
+java  -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimReplication
 rm *.class
