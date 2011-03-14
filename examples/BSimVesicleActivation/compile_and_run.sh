4e2b62a163dae00111dd016dce2675be1cc15458
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-03-14 13:51:04 +0000

    Updated examples

--- examples/BSimVesicleActivation/compile_and_run.sh
+++ examples/BSimVesicleActivation/compile_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-javac -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimBrownianMotion.java
-java  -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimBrownianMotion
+javac -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimVesicleActivation.java
+java  -cp .:../../lib/core.jar:../../lib/vecmath.jar:../../lib/objimport.jar:../../lib/bsim3.0.jar BSimVesicleActivation
 rm *.class
