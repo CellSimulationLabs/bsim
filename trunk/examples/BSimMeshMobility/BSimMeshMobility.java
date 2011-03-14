f8616a44a3564d93d3a567ecadfc07c6d07b7da5
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-03-14 16:12:57 +0000

    Updated example, outputs results to file by default.

--- trunk/examples/BSimMeshMobility/BSimMeshMobility.java
+++ trunk/examples/BSimMeshMobility/BSimMeshMobility.java
@@ -1,60 +1,37 @@
-
+/**
+ * BSimMeshMobility.java
+ * 
+ * Simulation that illustrates the effect that complex microscale environments can have on bacterial
+ * motility. Work derived from the BCCS-Bristol iGEM 2010 project 'argEcoli', see the following URL
+ * for further details:
+ *                            http://2010.igem.org/Team:BCCS-Bristol
+ */
 
 import java.awt.Color;
-
-import bsim.geometry.BSimCollision;
-import bsim.geometry.BSimMesh;
-import bsim.geometry.BSimOBJMesh;
-import bsim.geometry.BSimSphereMesh;
-import bsim.geometry.BSimMeshUtils;
-
-import java.util.Random;
-import java.util.Vector;
-
-
-import java.util.Scanner;
-
-import javax.vecmath.Point3d;
+import java.io.File;
+import java.util.*;
 import javax.vecmath.Vector3d;
 import processing.core.PGraphics3D;
-import bsim.BSim;
-import bsim.OctreeNode;
-import bsim.BSimOctreeField;
-import bsim.BSimTicker;
-import bsim.draw.BSimP3DDrawer;
-import bsim.BSimOctreeChemicalField;
 
-//import bsim.mesh.BSimFVMesh;
-import bsim.geometry.BSimSphereMesh;
-import bsim.geometry.BSimTriangle;
-import bsim.ode.BSimOdeSolver;
-import bsim.ode.BSimOdeSystem;
-import bsim.particle.BSimBacterium;
-import bsim.particle.BSimVesicle;
+import bsim.*;
+import bsim.draw.*;
+import bsim.export.*;
+import bsim.particle.*;
+import bsim.geometry.*;
 
-import bsim.draw.BSimDrawer;
-import bsim.draw.BSimP3DDrawer;
-import bsim.export.BSimLogger;
-import bsim.export.BSimMovExporter;
-import bsim.export.BSimPngExporter;
-
-
-
-
-/**Simple simulation for use in presentation
- * 
- * @author tt9671
- *
- */
-class BSimMeshMobility{
+class BSimMeshMobility {
 	static BSimOBJMesh theMesh;
 	public static void main(String args[]){
+		
+		/**
+		 * Create a new simulation object
+		 */
 		BSim sim = new BSim();		
-		sim.setDt(0.1);
+		sim.setDt(0.2);
 		sim.setTimeFormat("0.00");
-		sim.setSimulationTime(10);
+		sim.setSimulationTime(1000.0);
 		sim.setSolid(true,true,true);
-		sim.setBound(100,100,100);
+		sim.setBound(100000,100000,100000);
 
 		/**
 		 * Load the mesh (ensure only one load is uncommented)
@@ -67,10 +44,12 @@ class BSimMeshMobility{
 		/**
 		 * Place mesh in the correct location for the simulation
 		 */
-		theMesh.scale(53);
-		theMesh.translateAbsolute(new Vector3d(50.0, 50.0, 50.0));
+		theMesh.scale(55000);
+		theMesh.translateAbsolute(new Vector3d(50000,50000,50000));
 		
-	
+		/**
+		 * A type of bacteria that is repelled by the mesh surface
+		 */
 		class BSimCollidingBacterium extends BSimBacterium{			
 			public BSimCollidingBacterium(BSim sim, Vector3d position) {
 				super(sim, position);
@@ -81,22 +60,21 @@ class BSimMeshMobility{
 				BSimCollision.collideAndRepel(this, theMesh);
 			}
 		}
-		
-		final Vector<BSimCollidingBacterium> bacteria = new Vector<BSimCollidingBacterium>();
 
-		
-			//Set of variables to store the start positions; 
-			final double Xco=Math.random()*100;
-			final double Yco=Math.random()*100;
-			final double Zco=Math.random()*100;
-			
-			final BSimCollidingBacterium b = new BSimCollidingBacterium(sim, new Vector3d(Xco,Yco,Zco));
+		/**
+		 * Create set of bacteria for the simulation. All start at same location (centre) but
+		 * due to random nature of the run-and-tumble movement will search the space 
+		 * independantly.
+		 */
+		final Vector<BSimCollidingBacterium> bacteria = new Vector<BSimCollidingBacterium>();	
+		while(bacteria.size() < 1000) {		
+			BSimCollidingBacterium b = new BSimCollidingBacterium(sim, new Vector3d(50000.0,50000.0,50000.0));
 			bacteria.add(b);
+		}
 
-				
-		
-		
-
+		/**
+		 * Create the ticker for the simulation
+		 */
 		sim.setTicker(new BSimTicker() {
 			@Override
 			public void tick() {
@@ -107,7 +85,9 @@ class BSimMeshMobility{
 			}
 		});
 		
-		//creates a drawer
+		/**
+		 * Create the drawer
+		 */
 		BSimP3DDrawer drawer = new BSimP3DDrawer(sim, 800, 600) {
 			@Override
 			public void scene(PGraphics3D p3d) {	
@@ -117,35 +97,43 @@ class BSimMeshMobility{
 				draw(theMesh, 0);	
 			}
 		};			
-		
 		sim.setDrawer(drawer);
-	//	BSimPngExporter PNGExp = new BSimPngExporter(sim,drawer,"results/Move2");
 		
-		
-	//	sim.addExporter(PNGExp);
-		
-		BSimLogger trackerXY = new BSimLogger(sim, "results/trackerXY.csv") {
+		/**
+		 * Create exporter of the distance traveled from start position
+		 */
+		BSimLogger tracker = new BSimLogger(sim, "results.csv") {
+			@Override
+			public void before() {
+				int i = 1;
+				super.before();
+				String buffer = new String();
+				buffer = "time(seconds)";
+				for(BSimCollidingBacterium b : bacteria) {
+					buffer = buffer + ",Bac" + i;
+					i++;
+				}
+				write(buffer); 
+			}
 			@Override
 			public void during() {
-				//Logs distance moved from start point
-				double distance = Math.sqrt(Math.pow(Xco-b.getPosition().x,2)+Math.pow(Yco-b.getPosition().y,2)+Math.pow(Zco-b.getPosition().z,2));
-				
-				write(distance + "," + sim.getFormattedTime());
+				int i = 0;
+				String buffer = new String();
+				// Logs distance moved from start point
+				for(BSimCollidingBacterium b : bacteria) {
+					if (i > 0) buffer = buffer + ",";
+					buffer = buffer + Math.sqrt(Math.pow(50000.0-b.getPosition().x,2) + 
+						Math.pow(50000.0-b.getPosition().y,2) + 
+						Math.pow(50000.0-b.getPosition().z,2));
+					i++;
+				}
+				write(sim.getFormattedTime() + "," + buffer);
 			}
 		};
-		trackerXY.setDt(1.0);
-	//	sim.addExporter(trackerXY);
+		tracker.setDt(1.0);
+		sim.addExporter(tracker);
+		sim.export();
 
-	//	sim.export();
-		
-		//sim.addExporter(trackerXY);
-		
-		//sim.export(); 
-		
-		sim.preview();
-
-		
-	}
-	
+		// sim.preview();	
+	}	
 }
-
