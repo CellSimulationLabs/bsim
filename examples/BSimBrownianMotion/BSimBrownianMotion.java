32f2b7d64575badcf11a8cd05a6efc41af6784ca
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-03-14 18:26:19 +0000

    Updating examples so that they can compile in Eclipse

--- examples/BSimBrownianMotion/BSimBrownianMotion.java
+++ examples/BSimBrownianMotion/BSimBrownianMotion.java
@@ -1,3 +1,4 @@
+package BSimBrownianMotion;
 
 import java.util.Calendar;
 import java.util.Vector;
--- examples/BSimChemicalActivation/BSimChemicalActivation.java
+++ examples/BSimChemicalActivation/BSimChemicalActivation.java
@@ -1,4 +1,4 @@
-package bsim.example;
+package BSimChemicalActivation;
 
 import java.awt.Color;
 import java.util.Vector;
