39d8925be5e74f0c0e5b39b254beb749e4cf127e
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-04-05 17:00:16 +0000

    Drafted multi-threaded ticker and associated example.

--- src/bsim/BSimThreadedTicker.java
+++ src/bsim/BSimThreadedTicker.java
@@ -1,32 +1,55 @@
 package bsim;
 
+import java.util.Vector;
 
-public abstract class BSimThreadedTicker extends BSimTicker implements Runnable {
+
+public abstract class BSimThreadedTicker extends BSimTicker {
 	
-	int threads;
-	
-	/**
-	 * Run each timestep, this is where you should update particle properties
-	 * by calling interaction(), action() and updatePosition() methods
-	 */
+	protected int threads;
+	protected Vector<BSimThreadedTickerWorker> workers;
+	protected Object notifier;
 	
 	BSimThreadedTicker (int totalThreads) {
 		threads = totalThreads;
+		// Create a pool of worker threads
+		notifier = new Object();
+		workers = new Vector<BSimThreadedTickerWorker>(totalThreads);
+		for (int i = 0; i < totalThreads; i++) { 
+			BSimThreadedTickerWorker worker = createWorker(i, totalThreads, notifier);
+			workers.add(worker);
+			Thread t = new Thread(worker);
+			t.run();
+		}
 	}
 	
 	@Override
-	public void tick() {
-		
-		
-		
+	final public void tick() {
+		sequentialBefore();
+		for (int i = 0; i < threads; i++) {
+			workers.get(i).trigger();
+		}
+		// Wait until all threads have finished their work
+		try {
+			notifier.wait();
+			BSimThreadedTickerWorker.resetBarrier();
+		}
+		catch (InterruptedException e) { /* Do Nothing */ }
+		sequentialAfter();
 	}
 	
-	public void run() {
-		
-		
-	}
+	/**
+	 * Overwrite these if you wish to do some sequential operations
+	 * before or after the parallel block. Empty by default.
+	 */
+	public void sequentialBefore() {}
+	public void sequentialAfter() {}
 	
-	public void sequentialStart() {}
-	public abstract void threadedTick(int threadID, int threads);
-	public void sequentialEnd() {}
+	/**
+	 * 
+	 * @param threadID the unique thread ID
+	 * @param threads the total number of threads that are being used
+	 * @param notifier should be sent to the worker constructor to signal when done
+	 * @return The worker that will be called in parallel
+	 */
+	public abstract BSimThreadedTickerWorker createWorker (int threadID, int threads, Object notifier);
 }
