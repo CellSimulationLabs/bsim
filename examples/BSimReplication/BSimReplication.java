8a6887e482d830a782b9041c71681cc03f831374
antmatyjajo <antmatyjajo@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-06-14 23:23:24 +0000

    more examples updated

--- examples/BSimReplication/BSimReplication.java
+++ examples/BSimReplication/BSimReplication.java
@@ -11,22 +11,38 @@ import bsim.BSimTicker;
 import bsim.draw.BSimP3DDrawer;
 import bsim.particle.BSimBacterium;
 
+/**
+ * Growth and replication test.</br>
+ * 
+ * Simple example of growing and replicating population of bacteria swimming in an environment.
+ * Visualisation and testing of built in {@link BSimBacterium} growth/replication methods.
+ */
 public class BSimReplication {
 
 	public static void main(String[] args) {
 
-		BSim sim = new BSim();		
+		/*********************************************************
+		 * Set simulation properties
+		 */
+		BSim sim = new BSim();
+		sim.setDt(0.5);
 						
+		/*********************************************************
+		 * Set up the bacteria
+		 */
 		final Vector<BSimBacterium> bacteria = new Vector<BSimBacterium>();
 		final Vector<BSimBacterium> children = new Vector<BSimBacterium>();
 		while(bacteria.size() < 1) {		
 			BSimBacterium b = new BSimBacterium(sim, new Vector3d(Math.random()*sim.getBound().x, Math.random()*sim.getBound().y, Math.random()*sim.getBound().z));
 			b.setRadius();
-			b.setSurfaceAreaGrowthRate(9);
+			b.setSurfaceAreaGrowthRate(0.05);
 			b.setChildList(children);
 			bacteria.add(b);		
 		}
 
+		/*********************************************************
+		 * Set up the ticker
+		 */
 		sim.setTicker(new BSimTicker() {
 			@Override
 			public void tick() {
@@ -39,7 +55,9 @@ public class BSimReplication {
 			}		
 		});
 
-
+		/*********************************************************
+		 * Set up the drawer
+		 */
 		BSimP3DDrawer drawer = new BSimP3DDrawer(sim, 800,600) {
 			@Override
 			public void scene(PGraphics3D p3d) {						
@@ -50,7 +68,7 @@ public class BSimReplication {
 		};
 		sim.setDrawer(drawer);				
 
-	
+		// Run the simulation
 		sim.preview();
 
 	}
