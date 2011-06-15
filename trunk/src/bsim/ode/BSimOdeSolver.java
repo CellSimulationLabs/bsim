e196646b9b82f6fc86c6eaac476e49d9bc7bf159
antmatyjajo <antmatyjajo@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-06-15 00:31:36 +0000

    javadoc comments for ode package

--- trunk/src/bsim/ode/BSimOdeSolver.java
+++ trunk/src/bsim/ode/BSimOdeSolver.java
@@ -1,61 +1,48 @@
-/**
- * BSimOdeSolver.java
- * 
- * Solver routines for numerical simulation of ODEs (Fixed time-step):
- * - Euler's method
- * - second order Runge-Kutta
- * - fourth order Runge-Kutta.
- * 
- * Each method will estimate the change of the dependent variable based
- * on the previous value of the dependent (y) and independent (x) variables, 
- * and return the new value of the dependent variable.
- * 
- * The methods are in order of increasing accuracy for a given time-step;
- * Euler's method is the most basic, but the fastest as a result of having to 
- * perform relatively few calculations, while the Runge-Kutta methods use an
- * intermediate trial step at the midpoint of an interval to cancel lower order 
- * error terms. 
- * 
- * If the time step is too large an Euler solution will quickly diverge from
- * the true solution, therefore it is recommended to use a higher order solution 
- * if the time-step cannot reasonably be decreased.
- * 
- * Author: Antoni Matyjaszkiewicz
- * 
- * Created: 10/08/2009
- * Updated: 14/08/2009
- */
-
 package bsim.ode;
 
 /*
  * Most likely these will have to be updated or overloaded (some more)
  * to be able to cope with stochastic odes [honeycutt: stochastic rk algorithms]
- * as it looks like this is the phase where random variables must be integrated.
  */
 
+/**
+ * Solver routines for numerical simulation of ODEs (Fixed time-step):
+ * <ul>
+ * <li>Euler's method</li>
+ * <li>second order Runge-Kutta</li>
+ * <li>fourth order Runge-Kutta</li>
+ * </ul>
+ * 
+ * Each method will estimate the change of the dependent variable based
+ * on the previous value of the dependent (y) and independent (x) variables, 
+ * and return the new value of the dependent variable.</br>
+ * 
+ * The methods are in order of increasing accuracy for a given time-step;
+ * Euler's method is the most basic, but the fastest as a result of having to 
+ * perform relatively few calculations, while the Runge-Kutta methods use an
+ * intermediate trial step at the midpoint of an interval to cancel lower order 
+ * error terms. </br>
+ * 
+ * If the time step is too large an Euler solution will quickly diverge from
+ * the true solution, therefore it is recommended to use a higher order solution 
+ * if the time-step cannot reasonably be decreased.
+ */
 public class BSimOdeSolver {
 	// ----------------- Euler solvers ------------------
 	// --------------------------------------------------
-
-	/** 
-	 * Numerically solve a single ODE with Euler's method
-	 * y = euler(ODE, x, y, step_size)
-	*/ 
-	public static double euler(BSimOdeSingle ode, double x, double y, double h){
-		// Next y value = time-step * (dy/dx)
-		y += h*ode.derivative(x, y);
-		
-		return y;
-	}
 	
 	/**
-	 *  Numerically solve an ODE system with Euler's method
-	 *  y = euler(ODE_system, x, y_vector, step_size)
+	 *  Numerically solve an ODE system with Euler's method.
+	 *  
+	 * @param odes The {@link BSimOdeSystem} to solve.
+	 * @param t Independent variable.
+	 * @param y Vector of dependent variables.
+	 * @param h Time step for ode solution.
+	 * @return Vector of dependent variables at next time step.
 	 */
-	public static double[] euler(BSimOdeSystem odes, double x, double[] y, double h){
+	public static double[] euler(BSimOdeSystem odes, double t, double[] y, double h){
 		int numEq = odes.getNumEq();
-		double[] dydx = odes.derivativeSystem(x, y);
+		double[] dydx = odes.derivativeSystem(t, y);
 		
 		for(int i = 0;i<numEq;i++){
 			y[i] += h*dydx[i];
@@ -67,29 +54,17 @@ public class BSimOdeSolver {
 
 	// ---------- Runge-Kutta 2nd order solvers ---------
 	// --------------------------------------------------
-
-	/** 
-	 * Numerically solve a single ODE with 2nd order Runge-Kutta method
-	 * y = rungeKutta23(ODE, x, y, step_size)
-	 */
-	public static double rungeKutta23(BSimOdeSingle ode, double x, double y, double h){
-		double k1 = 0.0, k2 = 0.0;
-		
-		// Intermediate trial step coefficients (k values)
-	    k1 = h*ode.derivative(x, y);
-		k2 = h*ode.derivative(x + 0.5*h, y + 0.5*k1);	
-		
-		// New y value
-		y += k2;
-		
-		return y;
-	}
 	
 	/** 
-	 * Numerically solve an ODE system with 2nd order Runge-Kutta method
-	 * y = rungeKutta23(ODE_system, x, y_vector, step_size)
+	 * Numerically solve an ODE system with 2nd order Runge-Kutta method.
+	 * 
+	 * @param odes The {@link BSimOdeSystem} to solve.
+	 * @param t Independent variable.
+	 * @param y Vector of dependent variables.
+	 * @param h Time step for ode solution.
+	 * @return Vector of dependent variables at next time step.
 	 */
-	public static double[] rungeKutta23(BSimOdeSystem odes, double x, double[] y, double h){
+	public static double[] rungeKutta23(BSimOdeSystem odes, double t, double[] y, double h){
 		int numEq = odes.getNumEq();
 		double[] dydx = new double[numEq];
 		double[] yNew = new double[numEq];
@@ -97,12 +72,12 @@ public class BSimOdeSolver {
 		double[] k2 = new double[numEq];	
 	
 		// Calculate all k1 values
-		dydx = odes.derivativeSystem(x, y);
+		dydx = odes.derivativeSystem(t, y);
 		for(int i=0;i<numEq;i++){ k1[i] = h*dydx[i]; }
 	    
 		// Calculate all k2 values
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + 0.5*k1[i]; }
-		dydx = odes.derivativeSystem(x + 0.5*h, yNew);
+		dydx = odes.derivativeSystem(t + 0.5*h, yNew);
 		for(int i=0;i<numEq;i++){ k2[i] = h*dydx[i]; }
 		
 		// Compute vector of new y values
@@ -115,31 +90,17 @@ public class BSimOdeSolver {
 
 	// ---------- Runge-Kutta 4th order solvers ---------
 	// --------------------------------------------------
-
-	/** 
-	 * Numerically solve a single ODE function with 4th order Runge-Kutta method
-	 * y = rungeKutta45(ODE, x, y, step_size)
-	 */
-	public static double rungeKutta45(BSimOdeSingle ode, double x, double y, double h){
-		double k1 = 0.0, k2 = 0.0, k3 = 0.0, k4 = 0.0;
-
-		// Intermediate trial step coefficients (k values)
-	    k1 = h*ode.derivative(x, y);
-		k2 = h*ode.derivative(x + 0.5*h, y + 0.5*k1);
-		k3 = h*ode.derivative(x + 0.5*h, y + 0.5*k2);
-		k4 = h*ode.derivative(x + h, y + k3);
-		
-		// New y value based on the trial steps
-		y += k1/6 + k2/3 + k3/3 + k4/6;
-		
-		return y;
-	}
 	
 	/**
-	 *  Numerically solve an ODE system with 4th order Runge-Kutta method
-	 *  y = rungeKutta45(ODE_system, x, y_vector, step_size)
+	 * Numerically solve an ODE system with 4th order Runge-Kutta method
+	 *  
+	 * @param odes The {@link BSimOdeSystem} to solve.
+	 * @param t Independent variable.
+	 * @param y Vector of dependent variables.
+	 * @param h Time step for ode solution.
+	 * @return Vector of dependent variables at next time step.
 	 */
-	public static double[] rungeKutta45(BSimOdeSystem odes, double x, double[] y, double h){
+	public static double[] rungeKutta45(BSimOdeSystem odes, double t, double[] y, double h){
 		int numEq = odes.getNumEq();
 		double[] dydx = new double[numEq];
 		double[] yNew = new double[numEq];
@@ -149,22 +110,22 @@ public class BSimOdeSolver {
 		double[] k4 = new double[numEq];		
 	
 		// Calculate all k1 values
-		dydx = odes.derivativeSystem(x, y);
+		dydx = odes.derivativeSystem(t, y);
 		for(int i=0;i<numEq;i++){ k1[i] = h*dydx[i]; }
 	    
 		// Calculate all k2 values
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + 0.5*k1[i]; }
-		dydx = odes.derivativeSystem(x + 0.5*h, yNew);
+		dydx = odes.derivativeSystem(t + 0.5*h, yNew);
 		for(int i=0;i<numEq;i++){ k2[i] = h*dydx[i]; }
 		
 		// Calculate all k3 values
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + 0.5*k2[i]; }
-		dydx = odes.derivativeSystem(x + 0.5*h, yNew);
+		dydx = odes.derivativeSystem(t + 0.5*h, yNew);
 		for(int i=0;i<numEq;i++){ k3[i] = h*dydx[i]; }
 
 		// Calculate all k4 values 
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + k3[i]; }
-		dydx = odes.derivativeSystem(x + h, yNew);
+		dydx = odes.derivativeSystem(t + h, yNew);
 		for(int i=0;i<numEq;i++){ 
 			k4[i] = h*dydx[i];
 		}
@@ -177,5 +138,3 @@ public class BSimOdeSolver {
 		return y;
 	}
 }
-
-
--- trunk/src/bsim/ode/BSimOdeSystem.java
+++ trunk/src/bsim/ode/BSimOdeSystem.java
@@ -1,37 +1,26 @@
-/**
- * BSimOdeSystem.java
- * 
- * Interface used for defining a system of ODEs.
- * Defines the ODEs, the number of equations and the initial conditions.
- * 
- * Author: Antoni Matyjaszkiewicz
- * 
- * Created: 10/08/2009
- * Updated: 12/08/2009
- */
-
 package bsim.ode;
 
-//TODO Should this and single ode be an abstract class instead?
-//TODO Improve implementation of numEq, and ICs (like in single ode interface)
-
+/**
+ * Interface used for defining a system of ODEs.
+ * Defines the ODEs, the number of equations and the initial conditions.
+ */
 public interface BSimOdeSystem {
 	
 	/** 
-	 * Define a system of derivatives
-	 * dy[0] = ..., dy[1] = ..., etc
+	 * Define a system of derivatives - </br>
+	 * dy[0] = ..., dy[1] = ..., etc</br>
 	 * then return dy[]
 	 */
 	public double[] derivativeSystem(double x, double[] y);
 	
     /**
-     * Should return the number of equations in the system
+     * Get the number of equations in the system
      * (Corresponding to the number in derivativeSystem)
      */
 	public int getNumEq();
 	
 	/**
-	 *  Returns the initial conditions: y1(0), y2(0), etc..
+	 * Get the initial conditions: y1(0), y2(0), etc..
 	 */
 	public double[] getICs();
 	
