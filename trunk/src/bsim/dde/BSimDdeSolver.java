772949dfb9fe08ca7559697ac22ecc78894e708d
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-07-21 17:53:31 +0000

    New simple DDE solver that stores a full history for a maxDelay. Be careful of memory requirements for this approach.

--- trunk/src/bsim/dde/BSimDdeSolver.java
+++ trunk/src/bsim/dde/BSimDdeSolver.java
@@ -1,5 +1,7 @@
 package bsim.dde;
 
+import java.util.Vector;
+
 import bsim.dde.BSimDdeSystem;
 
 /*
@@ -30,27 +32,55 @@ import bsim.dde.BSimDdeSystem;
  * if the time-step cannot reasonably be decreased.
  */
 public class BSimDdeSolver {
+	
+	public static Vector<double[]> getInitialState (BSimDdeSystem ddes, double h){
+		// Create the initial state (including history)
+		Vector<double[]> ys = new Vector<double[]>((int)(ddes.getMaxDelay()/h));
+		for (int i=0; i<ys.size(); i++){
+			ys.add(ddes.getICs());
+		}
+		ddes.setInitialHistory(ys);
+		return ys;
+	}
+	
+	public static double[] getDelayedState(Vector<double[]> ys, double h, double delay){
+		// Calculate the index for the delayed value (rounds down)
+		int i = (int)(delay/h);
+		// Return the correct delayed state
+		return ys.get(i);
+	}
+	
+	public static void shiftState(Vector<double[]> ys){
+		// Shift all elements and loose last
+		double[] y = new double[ys.get(0).length];
+		int s = ys.size();
+		for (int i=s-1; i > 0; i--){
+			ys.set(i, ys.get(i-1));
+		}
+		ys.set(0, y);
+	}
+	
 	// ----------------- Euler solvers ------------------
 	// --------------------------------------------------
 	
 	/**
-	 *  Numerically solve an ODE system with Euler's method.
+	 *  Numerically solve an DDE system with Euler's method.
 	 *  
-	 * @param odes The {@link BSimOdeSystem} to solve.
+	 * @param odes The {@link BSimDdeSystem} to solve.
 	 * @param t Independent variable.
-	 * @param y Vector of dependent variables.
+	 * @param ys Vector of dependent variables including history.
 	 * @param h Time step for ode solution.
-	 * @return Vector of dependent variables at next time step.
+	 * @return Vector of dependent variables at next time step including history.
 	 */
-	public static double[] euler(BSimDdeSystem odes, double t, double[] y, double h){
-		int numEq = odes.getNumEq();
-		double[] dydx = odes.derivativeSystem(t, y);
-		
+	public static Vector<double[]> euler(BSimDdeSystem ddes, double t, Vector<double[]> ys, double h){
+		int numEq = ddes.getNumEq();
+		double[] dydx = ddes.derivativeSystem(t, ys.get(0), ys);
+		BSimDdeSolver.shiftState(ys);
+		double[] y = ys.get(0);
 		for(int i = 0;i<numEq;i++){
 			y[i] += h*dydx[i];
 		}
-		
-		return y;
+		return ys;
 	}
 	
 
@@ -66,28 +96,32 @@ public class BSimDdeSolver {
 	 * @param h Time step for ode solution.
 	 * @return Vector of dependent variables at next time step.
 	 */
-	public static double[] rungeKutta23(BSimDdeSystem odes, double t, double[] y, double h){
-		int numEq = odes.getNumEq();
+	public static Vector<double[]> rungeKutta23(BSimDdeSystem ddes, double t, Vector<double[]> ys, double h){
+		int numEq = ddes.getNumEq();
 		double[] dydx = new double[numEq];
 		double[] yNew = new double[numEq];
 		double[] k1 = new double[numEq];
-		double[] k2 = new double[numEq];	
+		double[] k2 = new double[numEq];
+		double[] y = ys.get(0);
 	
 		// Calculate all k1 values
-		dydx = odes.derivativeSystem(t, y);
+		dydx = ddes.derivativeSystem(t, y, ys);
 		for(int i=0;i<numEq;i++){ k1[i] = h*dydx[i]; }
 	    
 		// Calculate all k2 values
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + 0.5*k1[i]; }
-		dydx = odes.derivativeSystem(t + 0.5*h, yNew);
+		dydx = ddes.derivativeSystem(t + 0.5*h, yNew, ys);
 		for(int i=0;i<numEq;i++){ k2[i] = h*dydx[i]; }
+		
+		BSimDdeSolver.shiftState(ys);
+		y = ys.get(0);
 		
 		// Compute vector of new y values
 		for(int i=0;i<numEq;i++){
 			y[i] += k2[i];
 		}
 		
-		return y;
+		return ys;
 	}
 
 	// ---------- Runge-Kutta 4th order solvers ---------
@@ -102,41 +136,45 @@ public class BSimDdeSolver {
 	 * @param h Time step for ode solution.
 	 * @return Vector of dependent variables at next time step.
 	 */
-	public static double[] rungeKutta45(BSimDdeSystem odes, double t, double[] y, double h){
-		int numEq = odes.getNumEq();
+	public static Vector<double[]> rungeKutta45(BSimDdeSystem ddes, double t, Vector<double[]> ys, double h){
+		int numEq = ddes.getNumEq();
 		double[] dydx = new double[numEq];
 		double[] yNew = new double[numEq];
 		double[] k1 = new double[numEq];
 		double[] k2 = new double[numEq];
 		double[] k3 = new double[numEq];
-		double[] k4 = new double[numEq];		
+		double[] k4 = new double[numEq];	
+		double[] y = ys.get(0);
 	
 		// Calculate all k1 values
-		dydx = odes.derivativeSystem(t, y);
+		dydx = ddes.derivativeSystem(t, y, ys);
 		for(int i=0;i<numEq;i++){ k1[i] = h*dydx[i]; }
 	    
 		// Calculate all k2 values
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + 0.5*k1[i]; }
-		dydx = odes.derivativeSystem(t + 0.5*h, yNew);
+		dydx = ddes.derivativeSystem(t + 0.5*h, yNew, ys);
 		for(int i=0;i<numEq;i++){ k2[i] = h*dydx[i]; }
 		
 		// Calculate all k3 values
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + 0.5*k2[i]; }
-		dydx = odes.derivativeSystem(t + 0.5*h, yNew);
+		dydx = ddes.derivativeSystem(t + 0.5*h, yNew, ys);
 		for(int i=0;i<numEq;i++){ k3[i] = h*dydx[i]; }
 
 		// Calculate all k4 values 
 	    for(int i=0;i<numEq;i++){ yNew[i] = y[i] + k3[i]; }
-		dydx = odes.derivativeSystem(t + h, yNew);
+		dydx = ddes.derivativeSystem(t + h, yNew, ys);
 		for(int i=0;i<numEq;i++){ 
 			k4[i] = h*dydx[i];
 		}
+		
+		BSimDdeSolver.shiftState(ys);
+		y = ys.get(0);
 		
 		// Compute vector of new y values
 		for(int i=0;i<numEq;i++){
 			y[i] += k1[i]/6 + k2[i]/3 + k3[i]/3 + k4[i]/6;
 		}
 
-		return y;
+		return ys;
 	}
 }
--- trunk/src/bsim/dde/BSimDdeSystem.java
+++ trunk/src/bsim/dde/BSimDdeSystem.java
@@ -1,5 +1,7 @@
 package bsim.dde;
 
+import java.util.Vector;
+
 /**
  * Interface used for defining a system of DDEs.
  * Defines the DDEs, the number of equations and the initial conditions.
@@ -11,7 +13,7 @@ public interface BSimDdeSystem {
 	 * dy[0] = ..., dy[1] = ..., etc</br>
 	 * then return dy[]
 	 */
-	public double[] derivativeSystem(double x, double[] y);
+	public double[] derivativeSystem(double x, double[] y, Vector<double[]> ys);
 	
     /**
      * Get the number of equations in the system
@@ -20,8 +22,19 @@ public interface BSimDdeSystem {
 	public int getNumEq();
 	
 	/**
+	 * Get the maximum delay for the system
+	 * @return maximum delay
+	 */
+	public double getMaxDelay();
+	
+	/**
 	 * Get the initial conditions: y1(0), y2(0), etc..
 	 */
 	public double[] getICs();
 	
+	/**
+	 * Sets the initial history when solving. 
+	 * @param history Vector of historic states (double[])
+	 */
+	public void setInitialHistory(Vector<double[]> ys);
 }
