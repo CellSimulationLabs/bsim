dac5fac4962f5d3eb33ce7cd2770320ae07081bd
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-07-23 14:10:22 +0000

    Changes to DDE solver and Hasty example.

--- examples/BSimDdeDynamics/BSimDdeDynamics.java
+++ examples/BSimDdeDynamics/BSimDdeDynamics.java
@@ -23,61 +23,57 @@ public class BSimDdeDynamics {
 		 
 		
 		/*********************************************************
-		 * Create a new simulation object
+		 * Create a new simulation object (the environment)
 		 */
-		BSim sim = new BSim();			// New Sim object
-		sim.setDt(0.1);				    // Simulation Timestep
-		sim.setSimulationTime(10); 		// Simulation Length (6000 = 100 minutes)
-		sim.setTimeFormat("0.00");		// Time Format for display
-		sim.setBound(100,100,100);		// Simulation Boundaries
+		BSim sim = new BSim();        // New Sim object
+		sim.setDt(0.1);               // Simulation Timestep
+		sim.setSimulationTime(3600);  // Simulation Length (3600 seconds = 10 minutes)
+		sim.setTimeFormat("0.00");    // Time Format for display
+		sim.setBound(100,100,100);    // Simulation Boundaries
 
 		
 		/*********************************************************
-		 * BSimBacterium with a repressilator inside
+		 * Define a new type of bacterium that uses a DDE GRN
 		 */
 		class BSimDdeBacterium extends BSimBacterium {
-			protected DdeDyanmics ddeGRN;	// Instance of internal class
-			protected Vector<double[]> s;				// Local values of ODE variables
+			protected HastyDdeDyanmics ddeGRN;  // Instance of internal class
+			protected Vector<double[]> s;       // Local values of ODE variables
 
-			/*
-			 * Constructor for a repressilator GRN bacterium.
-			 */
+			/* Constructor */
 			public BSimDdeBacterium(BSim sim, Vector3d position){
 				super(sim, position);
-				
-				// Create the parameters and initial conditions for the ODE system
-				ddeGRN = new DdeDyanmics();
+				// Create the internal DDE system and generate initial state for the DDE system
+				ddeGRN = new HastyDdeDyanmics();
 				s = BSimDdeSolver.getInitialState(ddeGRN, sim.getDt());
 			}
 			
-			/*
-			 * Action each time step: iterate ODE system for one time-step.
-			 */
+			/* Action each time step: iterate DDE system and transfer AHL */
 			@Override
 			public void action() {
-				
 				// Movement etc:
 				super.action();
-				
 				// Solve the ode system
-				// IMPORTANT: re-scale the time units correctly (GRN equations are in minutes, BSim works in seconds)
-				//BSimDdeSolver.rungeKutta45(repGRN, sim.getTime()/60, ys, sim.getDt()/60);
-				BSimDdeSolver.euler(ddeGRN, sim.getTime(), s, sim.getDt());
+				BSimDdeSolver.rungeKutta45(ddeGRN, sim.getTime(), s, sim.getDt());
 			}
 			
-			/*
-			 * Representation of the repressilator ODE system with quorum coupling
-			 */
-			class DdeDyanmics implements BSimDdeSystem{
+			/* Representation of the Hasty oscillator DDE model */
+			class HastyDdeDyanmics implements BSimDdeSystem{
 				
-				// The equations
+				// The DDE equations
 				public double[] derivativeSystem(double x, double[] y, Vector<double[]> ys) {
-					double[] dy = new double[2];
-					double[] tm1 = BSimDdeSolver.getDelayedState(ys, sim.getDt(), 1.0);
-					double[] tm2 = BSimDdeSolver.getDelayedState(ys, sim.getDt(), 2.0);
+					double[] dy = new double[3];
+					// Get the state for a 600 seconds delay
+					double[] tm1 = BSimDdeSolver.getDelayedState(ys, sim.getDt(), 600.0);
 					
+					// Parameters for the equations (time is in seconds)
+					
+					
+					// A (AiiA)
 					dy[0] = 1.1 * tm1[0];
-					dy[1] = 1.1 * tm2[1];
+					// I (LuxI)
+					dy[1] = 1;
+					// H_i (Internal AHL)
+					dy[2] = 2;
 					
 					return dy;
 				}
@@ -86,19 +82,17 @@ public class BSimDdeDynamics {
 				public double[] getICs() {
 					double[] ics = new double[2];
 					ics[0] = 1.0;
-					ics[1] = 1.1;	
+					ics[1] = 1.1;
 					return ics;
 				}
-		
-				public int getNumEq() {
-					return 2;
-				}
 				
-				public double getMaxDelay() {
-					return 3.0;
-					
-				}
+				// Number of equations in the DDE
+				public int getNumEq() { return 3; }
 				
+				// The maximum delay we require (600 seconds in this case)
+				public double getMaxDelay() { return 600.0; }
+				
+				// Constant history the same as the initial conditions
 				public void setInitialHistory(Vector<double[]> ys) {
 					for (int i=0; i<ys.size(); i++){
 						ys.set(i, this.getICs());
@@ -112,9 +106,8 @@ public class BSimDdeDynamics {
 		 * Create the vector of all bacteria used in the simulation 
 		 */
 		final Vector<BSimDdeBacterium> bacteria = new Vector<BSimDdeBacterium>();
-		
-		// Add randomly positioned bacteria to the vector
-		while(bacteria.size() < 200) {		
+		// Add randomly positioned bacteria
+		while(bacteria.size() < 3) {		
 			BSimDdeBacterium p = new BSimDdeBacterium(sim, 
 										  new Vector3d(Math.random()*sim.getBound().x,
 													   Math.random()*sim.getBound().y,
@@ -123,21 +116,24 @@ public class BSimDdeDynamics {
 		}
 
 		
-		/**********************************************************/
+		/*********************************************************
+		 * Tasks to perform after each tick of the simulation 
+		 */
 		sim.setTicker(new BSimTicker() {
 			@Override
 			public void tick() {
-				
-				// Update the bacteria at each time step
+				// Update all bacteria at each time step
 				for(BSimDdeBacterium p : bacteria) {
-					p.action();		
-					p.updatePosition();
+					p.action();          // This updates the internal DDE equation
+					p.updatePosition();  // Move the bacteria (if necessary)
 				}
 			}		
 		});
 
 		
-		/**********************************************************/
+		/*********************************************************
+		 * Drawer to display the simulation to the screen 
+		 */
 		BSimDrawer drawer = new BSimP3DDrawer(sim, 800,600) {
 			@Override
 			public void scene(PGraphics3D p3d) {
@@ -149,7 +145,9 @@ public class BSimDdeDynamics {
 		sim.setDrawer(drawer);	
 		
 		
-		/**********************************************************/
+		/*********************************************************
+		 * Preview the simulation (directly to screen)
+		 */
 		sim.preview();
 	}
 }
