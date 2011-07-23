a4287ec637c0485a8ceebd612b82930aa2906d11
tgorochowski <tgorochowski@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-07-23 14:51:22 +0000

    Updates to DDE example (still not working)

--- trunk/examples/BSimDdeDynamics/BSimDdeDynamics.java
+++ trunk/examples/BSimDdeDynamics/BSimDdeDynamics.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 import javax.vecmath.Vector3d;
 import processing.core.PGraphics3D;
 import bsim.BSim;
+import bsim.BSimChemicalField;
 import bsim.BSimTicker;
 import bsim.draw.BSimDrawer;
 import bsim.draw.BSimP3DDrawer;
@@ -30,18 +31,28 @@ public class BSimDdeDynamics {
 		sim.setSimulationTime(3600);  // Simulation Length (3600 seconds = 10 minutes)
 		sim.setTimeFormat("0.00");    // Time Format for display
 		sim.setBound(100,100,100);    // Simulation Boundaries
+		
+		
+		/*********************************************************
+		 * Create a diffusive chemcial field for the AHL
+		 */
+		final double diffusivity = 100;    // Diffusivity of AHL
+		final double decayRate = 0.01/60;  // Decay Rate (0.1666 = 10 per minute, 0.01/60 = 1e-2 per min)
+		final BSimChemicalField field = new BSimChemicalField(sim, new int[]{25,25,25}, diffusivity, decayRate);
 
 		
 		/*********************************************************
 		 * Define a new type of bacterium that uses a DDE GRN
 		 */
 		class BSimDdeBacterium extends BSimBacterium {
-			protected HastyDdeDyanmics ddeGRN;  // Instance of internal class
-			protected Vector<double[]> s;       // Local values of ODE variables
+			protected HastyDdeDyanmics ddeGRN; // DDE based GRN
+			protected Vector<double[]> s;      // Current state, including history
+			protected double D = 2.5;          // Cell wall diffusion constant
 
 			/* Constructor */
 			public BSimDdeBacterium(BSim sim, Vector3d position){
 				super(sim, position);
+				
 				// Create the internal DDE system and generate initial state for the DDE system
 				ddeGRN = new HastyDdeDyanmics();
 				s = BSimDdeSolver.getInitialState(ddeGRN, sim.getDt());
@@ -50,10 +61,16 @@ public class BSimDdeDynamics {
 			/* Action each time step: iterate DDE system and transfer AHL */
 			@Override
 			public void action() {
-				// Movement etc:
+				// Movement, etc
 				super.action();
-				// Solve the ode system
+				
+				// Solve the DDE system
 				BSimDdeSolver.rungeKutta45(ddeGRN, sim.getTime(), s, sim.getDt());
+				
+				// Transfer AHL (In <-> Out)
+				double externalChem = field.getConc(position); // External chemical concentration
+				double deltaChem = externalChem - s.get(0)[2]; // The chemical gradient
+				field.addQuantity(position, D*(-deltaChem));   // Update the external field
 			}
 			
 			/* Representation of the Hasty oscillator DDE model */
@@ -63,13 +80,29 @@ public class BSimDdeDynamics {
 				public double[] derivativeSystem(double x, double[] y, Vector<double[]> ys) {
 					double[] dy = new double[3];
 					// Get the state for a 600 seconds delay
-					double[] tm1 = BSimDdeSolver.getDelayedState(ys, sim.getDt(), 600.0);
+					double[] yd = BSimDdeSolver.getDelayedState(ys, sim.getDt(), 600.0);
 					
 					// Parameters for the equations (time is in seconds)
+					double tFac = 60.0; // Factor used to ensure constants use time in seconds
+					double CA   = 1.0;
+					double CI   = 4.0;
+					double s    = 0.01;
+					double a    = 2500.0;
+					double k    = 1.0;
+					double k1   = 0.1;
+					double b    = 0.06;
+					double gA   = 15.0;
+					double gI   = 24.0;
+					double gH   = 0.01;
+					double f    = 0.3;
+					double g    = 0.01;
+					double d    = 1.0; // We consider a fixed population (TODO)
+					double d0   = 0.88;
 					
+					double P = (s + (a * yd[2]*yd[2])) / (1 + (k1 * yd[2]*yd[2]));
 					
 					// A (AiiA)
-					dy[0] = 1.1 * tm1[0];
+					dy[0] = 0;
 					// I (LuxI)
 					dy[1] = 1;
 					// H_i (Internal AHL)
