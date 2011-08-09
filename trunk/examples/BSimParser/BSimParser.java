10b7375d99aa96af97191096f7ff7966fdaa4611
tgorochowski@gmail.com <tgorochowski@gmail.com@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-08-09 20:17:42 +0000

    New example project that acts as a parser for a simulation definition file.

---
+++ trunk/examples/BSimParser/BSimParser.java
@@ -0,0 +1,20 @@
+package BSimParser;
+
+//import java.awt.Color;
+//import java.util.ArrayList;
+
+//import javax.vecmath.Vector3d;
+
+//import processing.core.PGraphics3D;
+//import bsim.*;
+//import bsim.draw.*;
+//import bsim.particle.*;
+
+
+public class BSimParser {
+
+	public static void main(String[] args) {
+
+
+	}
+}
