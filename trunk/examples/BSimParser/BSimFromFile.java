569c97355375853679621e83c4c39f322ef55876
tgorochowski@gmail.com <tgorochowski@gmail.com@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-08-20 21:30:07 +0000

    Getting started on the file parser example.

---
+++ trunk/examples/BSimParser/BSimFromFile.java
@@ -0,0 +1,74 @@
+package BSimParser;
+
+/* This file contains a representation of a simulation and is
+ * updated by the BSimParser.
+ */
+
+import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.*;
+
+import bsim.*;
+import bsim.particle.*;
+
+
+class BSimFromFile {
+	
+	// Simulation details
+	private BSim sim;
+	private HashMap<String, Vector<BSimBacterium>> bacteria;
+	private HashMap<String, Vector<BSimParticle>> particles;
+	
+	private String outputPath;
+	private String dataFileName;
+	private String movieFileName;
+	
+	public BSimFromFile () {
+		sim = new BSim();
+		bacteria = new HashMap<String, Vector<BSimBacterium>>();
+		particles = new HashMap<String, Vector<BSimParticle>>();
+	}
+	
+	
+	public void addBacteria (String name, Vector<BSimBacterium> newBacteria) { bacteria.put(name, newBacteria); }
+	
+	public void addParicles (String name, Vector<BSimParticle> newParticles) { particles.put(name, newParticles); }
+	
+	public void run() { sim.export(); }
+	
+	
+	
+// We have a set of inner classes that implement the various exporters and tickers for
+// our simulation.
+	
+	
+
+	class BSimFromFileTicker extends BSimTicker {
+		@Override
+		public void tick() {
+			
+			for (Map.Entry<String,Vector<BSimBacterium>> bacPop : bacteria.entrySet()) {
+				for(BSimBacterium bacterium : bacPop.getValue()) {
+					bacterium.action();
+					bacterium.updatePosition();
+				}
+			}
+			
+			for (Map.Entry<String,Vector<BSimParticle>> partPop : particles.entrySet()) {
+				for(BSimParticle particle : partPop.getValue()) {
+					particle.action();
+					particle.updatePosition();
+				}
+			}
+			
+		}
+	}
+	
+	
+	
+	
+	
+}
+
+
---
+++ trunk/examples/BSimParser/BSimParticleFactory.java
@@ -0,0 +1,17 @@
+package BSimParser;
+
+import java.util.Vector;
+import bsim.particle.BSimParticle;
+
+/**
+ * Factory to generate set of particles having been given a
+ * set of parameters from the file.
+ */
+class BSimParticleFactory {
+	
+	public static Vector<BSimParticle> parse (String params) {
+		
+		
+		return null;
+	}
+}
