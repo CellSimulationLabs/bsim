33c3482ec97d8ea637c2cf1bf538851421ff699b
tgorochowski@gmail.com <tgorochowski@gmail.com@1913bd76-7c48-11de-9afa-c7a50e5ca1b3>
2011-08-27 12:12:06 +0000

    BSimParser is now complete. Factory classes are still under development.

--- trunk/examples/BSimParser/BSimParser.java
+++ trunk/examples/BSimParser/BSimParser.java
@@ -45,18 +45,73 @@ public class BSimParser {
      * Use data following line header to set parameters in BSimFromFile object
      */
     private static void processLine(String[] line, BSimFromFile sim, int lineNo) {
-
-    	// For each type of parameter run the associated loader
+    	
+    	// --------------------------------------------------------------
+    	// Simulation Parameters (inc. meshes, chemical fields, etc)
+    	// --------------------------------------------------------------
     	
     	// Time Step
     	if (line[0].equals("DT")) 
     		sim.getSim().setDt(parseToDouble(line[1]));
-
+    	
     	// Create a new chemical field, options of the form CHEM_FIELD:name:param1=value1,param2=value2...
+    	// See BSimChemicalFieldFactory for more details.
+    	else if(line[0].equals("MESH"))
+    		sim.setMesh(line[1]);
+    	
+    	// Create a new bacterial population, options of the form BACTERIA:name:param1=value1,param2=value2...
+    	// See BSimChemicalFieldFactory for more details.
+    	else if(line[0].equals("BACTERIA"))
+    		sim.addBacteria(line[1], BSimBacteriumFactory.parse(line[2]));
+    	
+    	// Create a new particle population, options of the form PARTICLES:name:param1=value1,param2=value2...
+    	// See BSimChemicalFieldFactory for more details.
+    	else if(line[0].equals("PARTICLES"))
+    		sim.addParticles(line[1], BSimParticleFactory.parse(line[2]));
+    	
+    	// Create a new chemical field, options of the form CHEM_FIELD:name:param1=value1,param2=value2...
+    	// See BSimChemicalFieldFactory for more details.
     	else if(line[0].equals("CHEM_FIELD"))
     		sim.addChemicalField(line[1], BSimChemicalFieldFactory.parse(line[2]));
     	
-    	// Add all the other possible inputs here
+    	// --------------------------------------------------------------
+    	// Output Related Parameters
+    	// --------------------------------------------------------------
+    	
+    	else if (line[0].equals("OUTPUT_PATH")) 
+    		sim.setOutputPath(line[1]);
+    	
+    	else if (line[0].equals("OUTPUT_DATA")) {
+    		if (parseToInt(line[1]) == 1) { sim.setOutputData(true); }
+    		else { sim.setOutputData(false); }
+    	}
+    	
+    	else if (line[0].equals("OUTPUT_MOVIE")) {
+    		if (parseToInt(line[1]) == 1) { sim.setOutputMovie(true); }
+    		else { sim.setOutputMovie(false); }
+    	}
+    	
+    	else if (line[0].equals("DATA_FILENAME")) 
+    		sim.setDataFileName(line[1]);
+    	
+    	else if (line[0].equals("MOVIE_FILENAME")) 
+    		sim.setMovieFileName(line[1]);
+    	
+    	else if (line[0].equals("MOVIE_SPEED")) 
+    		sim.setMovieSpeed(parseToInt(line[1]));
+    	
+    	else if (line[0].equals("MOVIE_DT")) 
+    		sim.setMovieDt(parseToDouble(line[1]));
+    	
+    	else if (line[0].equals("MOVIE_WIDTH")) 
+    		sim.setMovieWidth(parseToInt(line[1]));
+    	
+    	else if (line[0].equals("MOVIE_HEIGHT")) 
+    		sim.setMovieHeight(parseToInt(line[1]));
+    	
+    	// --------------------------------------------------------------
+    	// Unknown Parameters
+    	// --------------------------------------------------------------
     	
     	// Generic comments of lines that are not use
     	else if(line[0].equals("***")) { } // Do nothing - comment
@@ -72,6 +127,19 @@ public class BSimParser {
     	catch(Exception e){ 
     		System.err.println("Problem converting to double");
     		result = 0.0;
+    	}
+    	return result;
+    }
+    
+    /**
+     * Convert String into an int handling possible errors (returning 0 if problem found)
+     */
+    private static int parseToInt(String str) {
+    	int result;
+    	try{ result = Integer.parseInt(str); }
+    	catch(Exception e){ 
+    		System.err.println("Problem converting to int");
+    		result = 0;
     	}
     	return result;
     }
